2bd2da7d5c9c9564b74fd75071501b9f00cf8b25
Tyler Watson <tyler@tw.id.au>
2018-04-30 01:29:22 +1000

    protoplugin: converted to dotnet core protoplugin: use OTAPI loader

---
+++ contrib/protoplugin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Steamworks;
+
+namespace protoplugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SteamUtils.IsOverlayEnabled();
+            try
+            {
+            Terraria.Program.ForceLoadAssembly(typeof(Terraria.Program).Assembly, true);
+            }
+            catch (ReflectionTypeLoadException rte)
+            {
+                Console.WriteLine(rte);
+                foreach (var ex in rte.LoaderExceptions)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            Directory.CreateDirectory("data");
+
+            ProtoPlugin.DumpTileFlags("data/tile/flags.dat");
+        }
+    }
+}
--- contrib/protoplugin/ProtoPlugin.cs
+++ contrib/protoplugin/ProtoPlugin.cs
@@ -1,34 +1,13 @@
 ﻿using System;
-using TerrariaApi.Server;
 using Terraria;
 using System.IO;
 using Google.Protobuf;
 
 namespace protoplugin
 {
-    [ApiVersion(1,22)]
-	public class ProtoPlugin : TerrariaPlugin
+	public static class ProtoPlugin
 	{
-		public override string Author => "Tyler W.";
-		public override string Description => "Dumps static data from Terraria.Main into protocol buffers for use with paper-tiger.";
-		public override bool Enabled => true;
-		public override string Name => "ProtoPlugin";
-		public override Version Version => new Version(1,0,0,0);
-
-		public ProtoPlugin (Main game) : base(game)
-		{
-		}
-
-		public override void Initialize ()
-		{
-            Directory.CreateDirectory("data");
-
-			ServerApi.Hooks.GamePostInitialize.Register(this, (EventArgs args) => {
-                DumpTileFlags("data/tile/flags.dat");
-			});
-		}
-
-		protected void DumpTileFlags(string path)
+		public static void DumpTileFlags(string path)
 		{
 			tile_flags tf = new tile_flags();
 
